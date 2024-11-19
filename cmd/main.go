6cb5e121e18b30a78e74554ddc4c63b50c680353
hypochloremic <26204559+HypoChloremic@users.noreply.github.com>
2024-11-19 23:59:03 +0100

    maingo update import

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"html/template"
 	"io"
 	"net/http"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
@@ -27,13 +26,13 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Static("/static", "static")
-	
+
 	t := NewTemplates()
 	e.Renderer = t
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index", nil)
 	})
-	
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
