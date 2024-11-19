02b4409d88bb0a225b1e03bc67897928de049f08
hypochloremic <26204559+HypoChloremic@users.noreply.github.com>
2024-11-19 23:45:11 +0100

    main golang templ

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Static("/static", "static")
+	
+	t := NewTemplates()
+	e.Renderer = t
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", nil)
+	})
+	
+	e.Logger.Fatal(e.Start(":8080"))
+}
